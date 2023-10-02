ba878a6da49d28547314e0bca234eed3664b3715
mark <anderkennedy@hotmail.com>
2023-10-01 22:05:00 -0300

    modificacion de tablas con PK

--- src/main/java/com/app/bematdid/model/DetalleCompraPk.java
+++ src/main/java/com/app/bematdid/model/DetalleCompraPk.java
@@ -2,8 +2,15 @@ package com.app.bematdid.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Embeddable
 public class DetalleCompraPk {
 
