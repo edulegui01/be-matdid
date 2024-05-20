6898a303fa3fd01cdb0de68bda702647f784e172
eduardo <edulegui01@gmail.com>
2024-05-20 11:35:21 -0400

    volver

--- src/main/java/com/app/bematdid/service/ProductoService.java
+++ src/main/java/com/app/bematdid/service/ProductoService.java
@@ -4,18 +4,10 @@ import com.app.bematdid.dto.ProductoDTO;
 import com.app.bematdid.mapper.ProductoMapper;
 import com.app.bematdid.model.Producto;
 import com.app.bematdid.repository.ProductoRepository;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.TypedQuery;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -25,7 +17,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -33,97 +24,23 @@ import java.util.UUID;
 @Service
 public class ProductoService {
 
-    @Autowired
-    private final EntityManager entityManager;
-
-
     private final static String UPLOADS_FOLDER = "uploads";
 
     private final static String URL_GET_IMAGE = "http://localhost:8090/producto/imagen?searchImagen=";
 
     @Autowired
     private ProductoRepository productoRepository;
-<<<<<<< HEAD
     private ProductoMapper productoMapper = new ProductoMapper();
-
-    public ProductoService(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
-=======
-
-    @Autowired
-    private ProductoMapper productoMapper;
->>>>>>> 89f2421fbc06e7cdfc19ee620df2e8be3e44962f
-
     public Page<ProductoDTO> listar(Pageable pageable, String nombre){
-        Page<Producto> productos = productoRepository.listarProducto(pageable, nombre);
-        return new PageImpl<>(productoMapper.productosAProductosDTO(productos.getContent()), pageable, productos.getTotalElements());
-    }
 
-<<<<<<< HEAD
         Page<Producto> resultPage = productoRepository.listarProducto(pageable, nombre);
+
+
+
         return productoMapper.mapEntityPageIntoDTOPage(pageable,resultPage);
 
-=======
-    public List<ProductoDTO> listarSelect(String search){
->>>>>>> 89f2421fbc06e7cdfc19ee620df2e8be3e44962f
-
-        return productoMapper.productosAProductosDTO(productoRepository.listarSelect(search));
-
-    }
-
-<<<<<<< HEAD
-    public Page<Producto> getListProduct(Pageable pageable, String nombreProducto, String idCiclo){
-        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-        CriteriaQuery<Producto> query = criteriaBuilder.createQuery(Producto.class);
-        Root<Producto> productoRoot = query.from(Producto.class);
-
-        List<Predicate> searchCriteria = new ArrayList<>();
-
-        //se agrega el where estado = true
-        searchCriteria.add(criteriaBuilder.isTrue(productoRoot.get("estado")));
-
-        if(nombreProducto != null && !nombreProducto.isEmpty()){
-            searchCriteria.add(criteriaBuilder.like(criteriaBuilder.upper(productoRoot.get("nombre")),"%"+nombreProducto.toUpperCase()+"%"));
-        }
-        if(idCiclo != null && !idCiclo.isEmpty()){
-            searchCriteria.add(criteriaBuilder.equal(productoRoot.get("idCiclo"),idCiclo));
-        }
-
-        Predicate[] restriccions = searchCriteria.toArray(new Predicate[0]);
-        query.select(productoRoot).where(criteriaBuilder.and(restriccions)).orderBy(criteriaBuilder.
-                desc(productoRoot.get("idProducto")));
-
-        TypedQuery<Producto> queryResult = entityManager.createQuery(query);
-
-        int total =queryResult.getResultList().size();
-
-        List<Producto> pagedData = paginateQuery(entityManager.createQuery(query), pageable).getResultList();
 
 
-
-        //calcular cantidad de item en la consulta
-        /*CriteriaQuery<Long> countQuery = criteriaBuilder.createQuery(Long.class);
-        Root<Producto> productoCountRoot = countQuery.from(Producto.class);
-        countQuery
-                .select(criteriaBuilder.count(productoCountRoot))
-                .where(criteriaBuilder.
-                        and(restriccions));
-        var totalCount = entityManager.createQuery(countQuery).getSingleResult();*/
-
-        return new PageImpl<>(pagedData,pageable,total);
-
-
-
-
-    }
-
-    public static <T> TypedQuery<T> paginateQuery(TypedQuery<T> query, Pageable pageable) {
-        if (pageable.isPaged()) {
-            query.setFirstResult((int) pageable.getOffset());
-            query.setMaxResults(pageable.getPageSize());
-        }
-        return query;
     }
 
     public List<Producto> listarSelect(String search){
@@ -136,38 +53,29 @@ public class ProductoService {
 
 
 
-=======
-    public Page<ProductoDTO> listarPorEditorial (Pageable pageable, int idEditorial){
-        Page<Producto> productos = productoRepository.listarPorEditorial(pageable,idEditorial);
-        return new PageImpl<>(productoMapper.productosAProductosDTO(productos.getContent()), pageable, productos.getTotalElements());
->>>>>>> 89f2421fbc06e7cdfc19ee620df2e8be3e44962f
     }
 
-    public Page<ProductoDTO> listarPorCategoria (Pageable pageable, int idCategoria){
-        Page<Producto> productos = productoRepository.listarPorCategoria(pageable,idCategoria);
-        return new PageImpl<>(productoMapper.productosAProductosDTO(productos.getContent()), pageable, productos.getTotalElements());
-    }
+    public void guardar(Producto producto){
 
-    public Page<ProductoDTO> listarPorCiclo (Pageable pageable, int idCiclo){
-        Page<Producto> productos = productoRepository.listarPorCiclo(pageable,idCiclo);
-        return new PageImpl<>(productoMapper.productosAProductosDTO(productos.getContent()), pageable, productos.getTotalElements());
-    }
-
-    public Page<ProductoDTO> listarPorMateria (Pageable pageable, int idMateria){
-        Page<Producto> productos = productoRepository.listarPorMateria(pageable,idMateria);
-        return new PageImpl<>(productoMapper.productosAProductosDTO(productos.getContent()), pageable, productos.getTotalElements());
-    }
-
-    public ProductoDTO guardar(ProductoDTO productoDTO){
-
-        Producto producto = productoMapper.productoDTOAProducto(productoDTO);
-        return productoMapper.productoAProductoDTO(productoRepository.save(producto));
+        productoRepository.save(producto);
     }
 
 
     public  Producto actualizar(Producto request,Long id){
+        Optional<Producto> productos = productoRepository.findById(id);
 
-        return productoRepository.save(request);
+        Producto producto = productos.get();
+        producto.setNombre(request.getNombre());
+        producto.setCosto(request.getCosto());
+        producto.setPrecio(request.getPrecio());
+        producto.setIva(request.getIva());
+        producto.setStockActual(request.getStockActual());
+
+
+
+        return productoRepository.save(producto);
+
+
 
     }
 
