0de84cc7b3c87c31659e9d691633408b04f55a87
mark <anderkennedy@hotmail.com>
2024-06-17 22:06:53 -0400

    Se creo el reporte de factura

--- src/main/java/com/app/bematdid/controller/FacturaController.java
+++ src/main/java/com/app/bematdid/controller/FacturaController.java
@@ -4,8 +4,10 @@ import com.app.bematdid.dto.FacturaDTO;
 import com.app.bematdid.error.StockNegativeException;
 import com.app.bematdid.service.FacturaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -33,5 +35,10 @@ public class FacturaController {
     @DeleteMapping("factura/anular/{idFactura}")
     public void anular (@PathVariable Long idFactura){
         facturaService.borrar(idFactura);
+    }
+
+    @GetMapping("factura/imprimir")
+    public ResponseEntity<Resource> imprimirFactura(@RequestParam long idFactura){
+        return facturaService.imprimirFactura(idFactura);
     }
 }
